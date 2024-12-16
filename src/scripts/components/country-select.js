f39880a3d8f71e5ce49362ba4faa1ff6dfe0fa81
Jiheng Nie <jiheng@seedcms.com>
2024-12-15 19:42:36 -0800

    Saving work

--- src/scripts/components/country-select.js
+++ src/scripts/components/country-select.js
@@ -4,19 +4,36 @@ class CountrySelect extends HTMLElement {
 
     this._selectors = {
       form: '[js-country-form]',
-      select: '[js-country-select]'
+      select: '[js-country-select]',
+      languageSelect: '[js-language-select]',
+      languageInput: '[js-language-input]',
+      languageInputLabel: '[js-language-input-label]',
+      countryLabel: '[js-country-input-label]'
     }
   }
 
   connectedCallback() {
     this.form = this.querySelector(this._selectors.form);
     this.select = this.querySelector(this._selectors.select);
+    this.languageSelect = this.querySelector(this._selectors.languageSelect)
+    this.languageInput = this.querySelector(this._selectors.languageInput)
+    this.languageInputLabel = this.querySelector(this._selectors.languageInputLabel)
+    this.countryLabel = this.querySelector(this._selectors.countryLabel)
 
     this.select.addEventListener('change', this._submitForm);
+    if (this.languageSelect) {
+      this.languageSelect.addEventListener('change', this._handleLanguageChange.bind(this));
+    }
   }
 
   _submitForm = () => {
-    console.log('change')
     this.form.submit();
+  }
+
+  _handleLanguageChange(e) {
+    this.languageInput.value = e.target.value
+    if (this.languageInputLabel) {
+      this.languageInputLabel.innerHTML =  e.target.options[e.target.selectedIndex].dataset.endonymName
+    }
   }
 }
