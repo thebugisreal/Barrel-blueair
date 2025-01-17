36e7774efb80fd83e5586a19118f73cc5f21de76
Jiheng Nie <jiheng@seedcms.com>
2025-01-16 17:14:46 -0800

    Julia QA - Homepage

--- src/scripts/components/section__image-caption-modal.js
+++ src/scripts/components/section__image-caption-modal.js
@@ -41,7 +41,7 @@ class ImageCaptionModal extends HTMLElement {
 
       if (window.innerWidth > 1025) {
         this.style.width='250px';
-        this.style.height='450px';
+        this.style.height='auto';
       } else {
         let closestImageContainer = this.parentElement.parentElement
         let width =  closestImageContainer.getBoundingClientRect().width - 48
