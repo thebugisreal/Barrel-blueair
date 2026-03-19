2fab86376d55c9d58b60b03662889fdccfe9e047
Hugh Bui <hugh.bui@barreldev.com>
2026-03-19 17:38:27 +0700

    BEA-71 Add loader animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -124,6 +124,17 @@ module.exports = {
       minHeight: {
         header: "calc(100vh - var(--header-height))"
       },
+      animation: {
+        "loader": "loaderLine 1.4s ease-in-out infinite",
+      },
+      keyframes: {
+        loaderLine: {
+          "0%": { transform: "translateX(-150%)" },
+          "45%": { transform: "translateX(0)" },
+          "55%": { transform: "translateX(0)" },
+          "to": { transform: "translateX(150%)" },
+        },
+      },
     },
   },
   borderRadius: {
