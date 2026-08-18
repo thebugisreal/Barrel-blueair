660be399e929e1adf08240254d9958076d241ccb
Ben <ben.than@barreldev.com>
2026-08-18 16:46:13 +0700

    BEA-95 Design - PDP > Above the fold redesign > Upsell > Hidden cart icon when added

--- src/scripts/components/product-upsell.js
+++ src/scripts/components/product-upsell.js
@@ -121,7 +121,7 @@ class ProductUpsell extends HTMLElement {
         this.cart.renderContents(response);
         this.cartDrawer.open();
         if (this.atcBtn && this.addedBtn) {
-          this.atcBtn.classList.add('hidden');
+          this.atcBtn.classList.add('product-upsell__atc--hidden');
           this.addedBtn.classList.remove('hidden');
         }
       })
@@ -176,14 +176,14 @@ class ProductUpsell extends HTMLElement {
       if (target.dataset.available == 'true') {
         if (btnText) btnText.textContent = 'Add to Cart';
         this.atcBtn.removeAttribute('disabled');
-        this.atcBtn.classList.remove('hidden');
+        this.atcBtn.classList.remove('product-upsell__atc--hidden');
         if (this.addedBtn) this.addedBtn.classList.add('hidden');
         if (this.bisBtn) this.bisBtn.classList.add('hidden');
       } else {
         if (btnText) btnText.textContent = 'Out of Stock';
         this.atcBtn.setAttribute('disabled', '');
         if (this.bisBtn) this.bisBtn.classList.remove('hidden');
-        this.atcBtn.classList.add('hidden');
+        this.atcBtn.classList.add('product-upsell__atc--hidden');
         if (this.addedBtn) this.addedBtn.classList.add('hidden');
       }
     }
